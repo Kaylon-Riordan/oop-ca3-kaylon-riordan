b15fd43bb80112d774473e75a4d6178b012987b6
Kaylon-Riordan <kaylonriordan@gmail.com>
2024-02-03 22:08:26 +0000

    Finished Question 4

--- src/CA3_Question4.java
+++ src/CA3_Question4.java
@@ -2,17 +2,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
+
 /**
  *  Name: Kaylon Riordan
  *  Class Group: GD2A
  */
+
 public class CA3_Question4 {
 
     /*
         filename: name of the file to test.
      */
+
     public static boolean validate(String filename) throws FileNotFoundException
     {
+        Scanner in = new Scanner(new File(filename));
+
+        Stack<String> tags = new Stack<>();
+
+        while(in.hasNext())
+        {
+            String tag = in.next();
+            if(!tags.isEmpty() && tag.equals(tags.peek().substring(0,1)+"/"+tags.peek().substring(1)))
+            {
+                tags.pop();
+            }
+            else
+            {
+                tags.push(tag);
+            }
+        }
+
+        if(tags.isEmpty())
+        {
+            return true;
+        }
         return false;
     }
 
@@ -21,9 +45,8 @@ public class CA3_Question4 {
          they are valid.
          tags_valid.txt should return true;
          tags_invalid.txt should output as invalid;
-
-
      */
+
     public static void main(String[] args) throws FileNotFoundException {
         String[] files = {"tags_valid.txt", "tags_invalid.txt"};
         for(String fName: files) {
