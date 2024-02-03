da9f94135393fb3963b127d487c013a9b7ee6e04
Kaylon-Riordan <kaylonriordan@gmail.com>
2024-02-03 21:08:18 +0000

    Finished Question 3

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Map;
 import java.util.TreeMap;
@@ -13,13 +14,43 @@ public class CA3_Question3
 {
     public static void readFile(String fileName) throws FileNotFoundException {
         Scanner in = new Scanner(new File(fileName));
-        in.useDelimiter("[^A-Za-z0-9_]+");
 
-        TreeMap<, > map = new TreeMap<>();
+        int count = 0;
+        String line, ident;
 
-        while(in.hasNext())
+        TreeMap<String, ArrayList<String>> identifiers = new TreeMap<>();
+
+        while(in.hasNextLine())
         {
-            System.out.println(in.next());
+            count++;
+            line = in.nextLine();
+
+            Scanner scan = new Scanner(line);
+            scan.useDelimiter("[^A-Za-z0-9_]+");
+
+            while(scan.hasNext())
+            {
+                ident = scan.next();
+                if(!identifiers.containsKey(ident))
+                {
+                    identifiers.put(ident, new ArrayList<>());
+                }
+                identifiers.get(ident).add(count + ":  " + line);
+            }
+        }
+
+        display(identifiers);
+    }
+
+    public static void display(TreeMap<String, ArrayList<String>> identifiers)
+    {
+        for (Map.Entry<String, ArrayList<String>> entry: identifiers.entrySet())
+        {
+            System.out.println("\n\nIdentifier '" + entry.getKey() + "' is used in these line(s): ");
+            for(int i = 0; i < entry.getValue().size(); i++)
+            {
+                System.out.println(entry.getValue().get(i));
+            }
         }
     }
 
