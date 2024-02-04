7ea3baaf899b3277e843280c75026edd0282b729
Kaylon-Riordan <kaylonriordan@gmail.com>
2024-02-04 19:08:18 +0000

    Finished Question 7

--- src/CA3_Question7.java
+++ src/CA3_Question7.java
@@ -1,8 +1,10 @@
 import java.util.*;
+
 /**
  *  Name: Kaylon Riordan
  *  Class Group: GD2A
  */
+
 public class CA3_Question7
 {
     /*
@@ -13,27 +15,91 @@ public class CA3_Question7
    or
    quit
     */
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
-        String command="";
+        String command = "";
+        Map<String, Queue<Block>> stock = new TreeMap<>();
+        int owned = 0;
+
         do {
-            System.out.print(">");
+            System.out.print("\n\n>");
             command = in.next();
+
             if(command.equalsIgnoreCase("buy"))
             {
-                String company = in.next();
+                String comp = in.next().toLowerCase();
                 int qty = in.nextInt();
                 double price = in.nextDouble();
-                // Code to buy shares here
+
+                if(!stock.containsKey(comp))
+                {
+                    stock.put(comp,  new LinkedList<>());
+                }
+
+                stock.get(comp).add(new Block(qty, price));
+                System.out.printf("\n%d shares of %s bought at $%.2f each.\n", qty, comp, price);
             }
-            else if(command.equals("sell"))
+            else if(command.equalsIgnoreCase("sell"))
             {
-                String company = in.next();
+                String comp = in.next().toLowerCase();
                 int qty = in.nextInt();
                 double price = in.nextDouble();
-                // Code to sell shares and calculate profit here
+
+                if(stock.containsKey(comp))
+                {
+                    owned = 0;
+                    for(Block b : stock.get(comp))
+                    {
+                        owned += b.getQuantity();
+                    }
+
+                    if(qty <= owned)
+                    {
+                        System.out.printf("\n%d shares of %s sold at $%.2f each.\n", qty, comp, price);
+
+                        double cost = 0;
+                        double gain = qty * price;
+
+                        while(qty > 0)
+                        {
+                            if(stock.get(comp).peek().getQuantity() > qty)
+                            {
+                                cost += qty * stock.get(comp).peek().getPrice();
+                                stock.get(comp).peek().setQuantity(stock.get(comp).peek().getQuantity() - qty);
+                                qty = 0;
+                            }
+                            else
+                            {
+                                cost += stock.get(comp).peek().getQuantity() * stock.get(comp).peek().getPrice();
+                                qty = qty - stock.get(comp).peek().getQuantity();
+                                stock.get(comp).remove();
+                            }
+                        }
+
+                        System.out.printf("Your profit is $%.2f.\n", gain - cost);
+                    }
+                    else
+                    {
+                        System.out.println("\nYou don't own that much stock in " + comp + ".");
+                    }
+                }
+                else
+                {
+                    System.out.println("\nYou don't own that much stock in " + comp + ".");
+                }
             }
-        }while(!command.equalsIgnoreCase("quit"));
+            System.out.println("\nYou now own");
+            for(String key : stock.keySet())
+            {
+                owned = 0;
+                for(Block b : stock.get(key))
+                {
+                    owned += b.getQuantity();
+                }
+                System.out.println(owned + " shares in " + key + ",");
+            }
+
+        } while(!command.equalsIgnoreCase("quit"));
     }
 }
