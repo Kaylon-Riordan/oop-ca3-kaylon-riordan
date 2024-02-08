11c364441a0c5de88b3c13db79a52de46032175e
Kaylon-Riordan <145358727+Kaylon-Riordan@users.noreply.github.com>
2024-02-08 21:19:38 +0000

    Finished question 8, fixed bugs with operator precedence

--- src/CA3_Question8.java
+++ src/CA3_Question8.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Stack;
 import static java.lang.Character.isDigit;
@@ -15,10 +14,10 @@ public class CA3_Question8 {
     public static void main(String[] args) {
         String equation;
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter equation: ");
+        System.out.println("Use the operators '+', '-', '*', '/', '(', ')', and numerical digits.\nPlease enter equation: ");
         equation = in.nextLine().trim();
 
-        Stack<Integer> numbers = new Stack<>();
+        Stack<Double> numbers = new Stack<>();
         Stack<Character> operators = new Stack<>();
 
         String num = "";
@@ -32,7 +31,7 @@ public class CA3_Question8 {
                 num = num + next;
                 if(i == equation.length()-1 || !isDigit(equation.charAt(i+1)))
                 {
-                    numbers.push(Integer.parseInt(num));
+                    numbers.push(Double.parseDouble(num));
                     num = "";
                 }
             }
@@ -42,7 +41,7 @@ public class CA3_Question8 {
             }
             else if(next == ')')
             {
-                while(operators.peek() != '(')
+                while(operators.peek() != '(' && numbers.size() > 1)
                 {
                     numbers.push(evaluate(numbers.pop(), numbers.pop(), operators.pop()));
                 }
@@ -50,7 +49,7 @@ public class CA3_Question8 {
             }
             else
             {
-                while(!operators.isEmpty() && operators.peek() > precedence(next))
+                while(!operators.isEmpty() && operators.peek() != '(' && precedence(operators.peek()) >= precedence(next) && numbers.size() > 1)
                 {
                     numbers.push(evaluate(numbers.pop(), numbers.pop(), operators.pop()));
                 }
@@ -66,10 +65,10 @@ public class CA3_Question8 {
                 }
             }
         }
-        System.out.println("\nAnswer = " + numbers.peek());
+        System.out.printf("\nAnswer = %.2f", numbers.peek());
     }
 
-    public static int evaluate(int b, int a, char op)
+    public static double evaluate(double b, double a, char op)
     {
         if(op == '+')
         {
@@ -91,108 +90,17 @@ public class CA3_Question8 {
 
     public static int precedence(char op)
     {
-        if (op == '/' || op == '*' || op == 'x' || op == 'X')
+        if(op == '*' || op == '/')
         {
-            return(2);
+            return 2;
         }
         else if(op == '+' || op == '-')
         {
-            return(1);
+            return 1;
         }
         else
         {
-            return(0);
+            return 0;
         }
     }
 }
-
-//
-//import java.util.Scanner;
-//        import java.util.Stack;
-//        import static java.lang.Character.isDigit;
-//
-///**
-// *  Name: Kaylon Riordan
-// *  Class Group: GD2A
-// */
-
-//public class CA3_Question8 {
-//    /*
-//        Reads in an equation from the user
-//     */
-//    public static void main(String[] args) {
-//        String equation;
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Please enter equation: ");
-//        equation = in.nextLine().trim();
-//
-//        Stack<Integer> numbers = new Stack<>();
-//        Stack<Character> operators = new Stack<>();
-//
-//        int a, b;
-//        int priority = 0;
-//        String num = "";
-//
-//
-//        for(int i = 0; i < equation.length(); i++)
-//        {
-//            char next = equation.charAt(i);
-//            if(next == '*' || next == 'x' || next == 'X' || next == '÷' || next == '/')
-//            {
-//                priority++;
-//            }
-//        }
-//        for(int i = 0; i < equation.length(); i++)
-//        {
-//            char next = equation.charAt(i);
-//
-//            if(isDigit(next))
-//            {
-//                num = num + next;
-//                if(i == equation.length()-1 && next != ')')
-//                {
-//                    numbers.push(Integer.parseInt(num));
-//                }
-//            }
-//            else
-//            {
-//                operators.push(next);
-//                if(num != "")
-//                {
-//                    numbers.push(Integer.parseInt(num));
-//                    num = "";
-//                }
-//            }
-//
-//            System.out.println("\n"+numbers+"\n"+operators);
-//
-//            if(numbers.size() > 1)
-//            {
-//                b = numbers.pop();
-//                a = numbers.pop();
-//
-//                if(operators.peek() == '+' && priority == 0)
-//                {
-//                    numbers.push(a + b);
-//                }
-//                else if(operators.peek() == '-' && priority == 0)
-//                {
-//                    numbers.push(a - b);
-//                }
-//                else if(operators.peek() == '*' || operators.peek() == 'x' || operators.peek() == 'X')
-//                {
-//                    numbers.push(a * b);
-//                    priority--;
-//                }
-//                else if(operators.peek() == '÷' || operators.peek() == '/')
-//                {
-//                    numbers.push(a / b);
-//                    priority--;
-//                }
-//                operators.pop();
-//            }
-//            System.out.println("\n"+numbers+"\n"+operators);
-//        }
-//        System.out.println("\nAnswer = " + numbers.peek());
-//    }
-//}
